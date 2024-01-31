122e67a605076066e777d147377147bb93c9c716
durga kiran <durgakiran2024@gmail.com>
2024-01-31 11:44:52 -0600

    initial

---
+++ info.js
@@ -0,0 +1,331 @@
+let info = {
+  name: "Durga Kiran",
+  logo_name: "Durga Kiran",
+  flat_picture: require("./src/assets/img1.jpg"),
+  config: {
+    use_cookies: true,
+    navbar: {
+      blur: false
+    }
+  },
+  description:
+    "I am Byrraju Bala Durga Kiran Raju, a software developer and engineer located in Warrensburg, Missouri. I have a Bachelor's degree in Electronics and Communication Engineering and have experience working as a UI/UX Developer at Mphasis where I developed web pages using JavaScript, jQuery, and AngularJS.<br><br>I am a detail oriented person and I keep my goals and tasks organized to maintain productivity. I would bring to table a wide range of software skills, ranging from design and development to writing scripts for intelligent agents. I believe that the skills that I have attained from the dynamic environment and competitive university life define me, and I am looking forward to implementing and polishing them in any opportunity that I receive. ",
+  links: {
+    linkedin: "https://www.linkedin.com/in/durga-kiran-byrraju/",
+    github: "https://github.com/BalaDurgaKiran",
+    //angellist: "",
+    resume:
+      "https://flowcv.com/resume/l88h98mr09"
+  },
+  education: [
+    {
+      name: "University of Central Missouri ",
+      place: "USA",
+      date: "Aug, 2023 - Current",
+      degree: "Masters in Industrial Technology",
+      //gpa: "3.7/4.0",
+      description:
+        "Currently pursuing my Master's in Industrial Technology, while specializing in the field of Web Development.",
+      skills: [
+        "Organizational dynamics ",
+        "Software Engineering",
+        "Algorithms",
+        "Statistics",
+       // "",
+       // "OS"
+      ]
+    },
+    {
+      name: "MLR Institute of Technology",
+      place: "India",
+      date: "Aug, 2017 - Sep, 2021",
+      degree: "Bachelors in Electronics and Communication Engineering",
+      //gpa: "8.0/10.0",
+      description:
+        "Graduated from MLR Institute of Technology, with a bachelors degree in Electronics and Communication Engineering.",
+      skills: [
+        "Software",
+        "Communication Engineering",
+        "Data Visualization",
+        //"Computer Vision",
+       // "Pattern Recognition",
+       // "HCI",
+       // "Software Engineering"
+      ]
+    }
+  ],
+ experience: [
+    {
+      name: "Mphasis Limited",
+      place: "Hyderabad, India",
+      date: "Dec, 2021 - Dec, 2022",
+      position: "Software Developer",
+      description:
+        "As an UI/UX developer, I specialize in designing and constructing user-friendly interfaces for complex web applications. My expertise spans comprehensive user research, interactive prototyping, information architecture, and visual design. I create aesthetically pleasing, responsive interfaces with a focus on visual hierarchy and consistency. Leveraging Angular, Typescript, and RxJS, I build adaptive web experiences with reusable components, ensuring cross-browser compatibility. I also enhance user interactions with animations to deliver a delightful user experience.",
+      skills: ["Angular", "Node.js", "Typescript", "Java scprit", "UI/UX"]
+    },
+    {
+      name: "Ajna Lens",
+      place: "Remote, India",
+      date: "Aug, 2022 - Feb, 2023",
+      position: "Ajna Creator",
+      description:
+        "I specialize in decentralized web technologies. With experience in immersive technologies and digital twin systems, I've worked on projects involving AR/VR, IoT, and analyzed the impact of emerging technologies in areas like supply chain and healthcare. Constantly staying current, I enhance my skills in Unity, Unreal Engine and Web3 to contribute to ongoing innovations in the immersive space.",
+      skills: ["Metaverse", "unreal engine 5", "3D Modeling", "Web3", "Character Animation"]
+    },
+
+   {
+      name: "verzeo edu tech ",
+      place: "Hyderabad, India",
+      date: "Nov, 2020 - Jan, 2021",
+      position: "Intern",
+      description:
+        "Conducted comprehensive data analysis and modeling, using Python and Tableau to visualize insights, resulting in actionable recommendations and improved organizational efficiency.",
+      skills: ["Python", "Tableau", "Data Analysis", "Data visualization"]
+    }
+     
+  ],
+  skills: [
+    {
+      title: "languages",
+      info: [
+        "Python",
+        "Javascript",
+        "TypeScript",
+        "Java",
+        "C",
+        //"C++",
+        "SQL",
+        //"NoSQL",
+        //"Ruby"
+      ],
+      icon: "fa fa-code"
+    },
+    {
+      title: "data frameworks",
+      info: [
+        "Keras",
+        "Numpy",
+        "Pandas",
+       // "OpenCV",
+        //"SciPy",
+        //"Sklearn",
+        //"LibROSA",
+        //"Pillow"
+      ],
+      icon: "fa fa-cubes"
+    },
+    {
+      title: "web technologies",
+      info: ["Vue", "Angular", "React", "Node", "HTML", "CSS"],
+      icon: "fas fa-laptop-code"
+    },
+    {
+      title: "databases",
+      info: ["MongoDB", "MySQL", "OracleSQL"],
+      icon: "fa fa-database"
+    },
+    {
+      title: "operating systems & tools",
+      info: [
+        "MacOS",
+        "Ubuntu",
+        "Windows",
+        "Android",
+        "Agile",
+       // "Scrum",
+       // "JIRA",
+       // "Heroku",
+        "Firebase",
+       // "AWS S3"
+      ],
+      icon: "fas fa-tools"
+    },
+    {
+      title: "design",
+      info: ["Illustrator", "XD", "Photoshop"],
+      icon: "fa fa-pencil-square-o"
+    }
+  ],
+  portfolio: [
+    {
+      name: "Vue-App",
+      pictures: [
+        {
+          img: require("./src/assets/vue.jpg")
+        },
+     
+      ],
+      technologies: ["Vue-Js","Java script" , "Webdevelopment"],
+      category: "Website",
+      date: "Dec, 2023 - Jan, 2024",
+      github:
+        "https://github.com/BalaDurgaKiran/vue-app",
+      visit: "https://github.com/BalaDurgaKiran/vue-app",
+      description:
+        "This project that serves as a testament to my newfound skills in Vue.js. This project allowed me to explore the Vue development frame work, enhance user experiences, and implement dynamic web applications."
+    },
+    {
+      name: "Medicare-Project",
+      pictures: [
+        {
+          img: require("./src/assets/medicare.jpg")
+        },
+        
+      ],
+      technologies: ["Node", "Angular", "Express", "MongoDB", "Json", "Java script"],
+      category: "Website",
+      date: "Dec, 2021 - Feb, 2022",
+      github: "https://github.com/BalaDurgaKiran/medicare-project",
+      visit: "https://github.com/BalaDurgaKiran/medicare-project",
+      description:
+        "Angular Medicare E-Commerce website. This platform is designed to provide a seamless and user-friendly experience for users seeking to purchase a wide range of Medicare products. Built with Angular, a robust and efficient front-end framework, our website ensures a responsive and dynamic interface for users across various devices."
+    },
+    {
+    /*  name: "Nutricare",
+      pictures: [
+        {
+          img: require("./src/assets/portfolio/nc/one.png")
+        },
+      ],
+      technologies: ["Node", "Angular", "Express", "MongDB", "Medical"],
+      category: "Website",
+      github: "",
+      date: "May, 2017 - Aug, 2017",
+      visit: "",
+      description:
+        "."*/
+    }
+  ],
+ portfolio_design: [
+    {
+      name: "Gradmate",
+      title: "Gradmate - Mockup Design",
+      pictures: [
+        {
+          img: require("./src/assets/gradmate-logo.jpg"),
+          title: "logo"
+        },
+        {
+          img: require("./src/assets/gradmate/9.jpg"),
+          title: "Mockups 5"
+        },
+        {
+          img: require("./src/assets/gradmate/1.jpg"),
+          title: "Mockups 1"
+        },
+        {
+          img: require("./src/assets/gradmate/2.jpg"),
+          title: "Mockups 2"
+        },
+        {
+          img: require("./src/assets/gradmate/3.jpg"),
+          title: "Mockups 3"
+        },
+        {
+          img: require("./src/assets/gradmate/4.jpg"),
+          title: "Mockups 4"
+        }
+        
+      ],
+      technologies: ["XD", "Illustrator"],
+      category: "Visual Design",
+      github: "",
+      date: "Oct, 2023 - Sep, 2023",
+      visit: "",
+      description:
+        "Elevate Your GPA Game with Grad Mate: Your Personal GPA Tracker for Academic Success!"
+    },
+    {
+      name: "Digital-Twin",
+      title: "Unreal Projects",
+      pictures: [
+        {
+          img: require("./src/assets/ue.jpg"),
+          title: "Unreal Engine"
+        },
+       
+      ],
+      technologies: ["3D", "Animation", "Digital Twin"],
+      category: "Visual Design",
+      github: "",
+      date: "Sep, 2022 - Feb, 2023",
+      visit: "",
+      description:
+        "I have leveraged Unreal Engine to meticulously craft digital twins and design immersive 3D assets for the Metaverse, seamlessly integrating technologies like AR, VR, MR, and NFTs."
+    },
+   /*{
+      name: "Bunder",
+      title: "Bunder - MVP Proposal",
+      pictures: [
+        {
+          img: require("./src/assets/designs/bunder/Moodboard.png"),
+          title: "Moodboard"
+        },
+        {
+          img: require("./src/assets/designs/bunder/Mockup 1.png"),
+          title: "Mockup 1"
+        },
+        {
+          img: require("./src/assets/designs/bunder/Mockup 2.png"),
+          title: "Mockup 2"
+        },
+        {
+          img: require("./src/assets/designs/bunder/Mockup 3.png"),
+          title: "Mockup 3"
+        },
+        {
+          img: require("./src/assets/designs/bunder/Market Research 1.png"),
+          title: "Market Research 1"
+        },
+        {
+          img: require("./src/assets/designs/bunder/Market Research 2.png"),
+          title: "Market Research 2"
+        }
+      ],
+      technologies: ["XD", "Illustrator"],
+      category: "Visual Design",
+      github: "",
+      date: "May, 2020 - July, 2020",
+      visit: "",
+      description:
+        "Bunder is an intuitive web platform for micro-communities to share sensitive data with privacy within the community and allow engagement amongst them. Our aim is two-fold - Co-existing and Collaboration. The former is executed by providing a platform for communities that are geographically positioned in the same proximity (Housing societies, Dorms, Apartment Complexes) to have and share data (exact apartment addresses, phone number, apartment housing rules, individuals tested positive for COVID-19) in an exclusive platform. The latter is to give these people in the community the ability to carry out a task you are physically or remotely unable to do; with dignity and ease."
+    }*/
+  ],
+  /*recommendations: [
+    {
+      title:
+        "",
+      author: "",
+      position: "",
+      company: "",
+      location: ""
+    },
+    {
+      title:
+        "",
+      author: "",
+      position: "",
+      company: "",
+      location: ""
+    },
+    {
+      title:
+        "",
+      author: "",
+      position: "",
+      company: "",
+      location: ""
+    },
+    {
+      title:
+        "",
+      author: "",
+      position: "",
+      company: "",
+      location: ""
+    }
+  ]*/
+};
+
+export default info;
